f573ff1a9f8fbc8c3dda57cdfcb5a45fcbb89786
muzaijian <18807718239@163.com>
2021-08-31 17:56:21 +0800

    初始化项目

---
+++ sa-token/src/main/java/cn/muzaijian/mall/satoken/SatokenApplication.java
@@ -0,0 +1,13 @@
+package cn.muzaijian.mall.satoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatokenApplication.class, args);
+    }
+
+}
