bd6b0d7272bfca462b77d9470a994d73a7807686
Jarrett <616766585@qq.com>
2022-02-15 22:39:09 +0800

    线程池测试

--- java-thread/src/main/java/com/muzaijian/zai/javathread/pool/MyRunnable.java
+++ java-thread/src/main/java/com/muzaijian/zai/javathread/pool/MyRunnable.java
@@ -1,4 +1,31 @@
 package com.muzaijian.zai.javathread.pool;
 
-public class MyRunnable {
+import java.util.Date;
+
+public class MyRunnable implements Runnable {
+
+    private final String command;
+
+    public MyRunnable(String s) {
+        this.command = s;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ", id:" + command + " Start. Time = " + new Date());
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + ", id:" + command + " End. Time = " + new Date());
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.command;
+    }
 }
