0b3e3416fd8a5611382e71dbbe7102458591a12e
muzaijian <18807718239@163.com>
2022-03-02 17:30:43 +0800

    修改线程测试

--- java-thread/src/main/java/com/muzaijian/zai/javathread/pool/ThreadPoolExecutorDemo.java
+++ java-thread/src/main/java/com/muzaijian/zai/javathread/pool/ThreadPoolExecutorDemo.java
@@ -6,13 +6,12 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutorDemo {
 
-    private static final int CORE_POOL_SIZE = 1;
-    private static final int MAX_POOL_SIZE = 2;
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAX_POOL_SIZE = 4;
     private static final int QUEUE_CAPACITY = 1;
     private static final Long KEEP_ALIVE_TIME = 1L;
 
     public static void main(String[] args) {
-
         //使用阿里巴巴推荐的创建线程池的方式
         //通过ThreadPoolExecutor构造函数自定义参数创建
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
@@ -21,36 +20,19 @@ public class ThreadPoolExecutorDemo {
                 KEEP_ALIVE_TIME,
                 TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(QUEUE_CAPACITY),
-                new ThreadPoolExecutor.CallerRunsPolicy());
+                new ThreadPoolExecutor.DiscardOldestPolicy());
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 10; i++) {
             //创建WorkerThread对象（WorkerThread类实现了Runnable 接口）
             System.out.println(i);
             Runnable worker = new MyRunnable("" + i);
             //执行Runnable
             executor.execute(worker);
         }
-
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(executor.getPoolSize());
-
-        for (int i = 0; i < 2; i++) {
-            //创建WorkerThread对象（WorkerThread类实现了Runnable 接口）
-            System.out.println(i);
-            Runnable worker = new MyRunnable("" + i);
-            //执行Runnable
-            executor.execute(worker);
-        }
-
 
         //终止线程池
-        executor.shutdown();
-        while (!executor.isTerminated()) {
+        executor.shutdownNow();
+        while (!executor.isShutdown()) {
         }
         System.out.println("Finished all threads");
     }
